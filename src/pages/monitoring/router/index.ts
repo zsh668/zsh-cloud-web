eb5632289d89bae0afcbf42bdd19501002fbe9c3
zhangshuhang <zhangshuhang@zsh6.com>
2022-07-13 17:38:09 +0800

    修改 成 动态路由

--- src/pages/monitoring/router/index.ts
+++ src/pages/monitoring/router/index.ts
@@ -1,33 +1,34 @@
 import Layout from '@/layout/index.vue'
 export default [
   {
-    path: '/monitor',
+    path: '/monitoring',
     component: Layout,
-    redirect: '/loginLlist',
+    redirect: '/log-login',
     meta: {
       title: '监控管理',
       icon: 'monitor'
     },
     children: [
       {
-        path: 'loginLog',
+        path: 'log-login',
         component: () =>
           import(
             /* webpackChunkName: "form" */ '@/pages/monitoring/log-login/index.vue'
-          ),
+            ),
         meta: {
-          title: '登录日志'
-          // icon: 'icon_menu_shenpi'
+          title: '登录日志',
+          icon: 'monitor'
         }
       },
       {
-        path: 'operation',
+        path: 'log-operation',
         component: () =>
           import(
             /* webpackChunkName: "form" */ '@/pages/monitoring/log-operation/index.vue'
-          ),
+            ),
         meta: {
-          title: '操作日志'
+          title: '操作日志',
+          icon: 'monitor'
           // icon: 'icon_menu_shenpi'
         }
       }
